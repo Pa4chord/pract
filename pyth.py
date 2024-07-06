1616c59bcfbc49f9479fccd0036d0e28914badb8
Daniil Serov <DanSer202@yandex.ru>
2024-07-06 20:40:23 +0300

    sss

--- pyth.py
+++ pyth.py
@@ -4,8 +4,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
 driver.get("https://ya.ru")
-search_box = driver.find_element(By.id,"searchbox")
+search_box = driver.find_element(By.NAME,"text")
 search_box.send_keys("Hello world")
-find_button = driver.find_element(By.class_name,"btn")
+find_button = driver.find_element(By.NAME,"search3_button mini-suggest_button")
 find_button.click()
 driver.quit()
